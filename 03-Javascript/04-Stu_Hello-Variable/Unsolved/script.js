821f7832c4be1d5a7e0b420ab5c9bd7a0475cb94
Gus Miller <gustavo.miller@miller-hs.com>
2023-08-16 21:31:38 -0400

    first code upload - working version: nav, header and hero

--- 03-Javascript/04-Stu_Hello-Variable/Unsolved/script.js
+++ 03-Javascript/04-Stu_Hello-Variable/Unsolved/script.js
@@ -1,1 +1,16 @@
 // Write Your JavaScript Code Here
+var personName = "Gustavo Miller";
+var pets = "parrot";
+var funFact = "Hockey seasson";
+
+console.log(personName);
+console.log(pets);
+console.log(funFact);
+
+personName = "Gus";
+pets = "loro";
+funFact = "follow the rabbit";
+
+console.log("My name is " + personName + " and I have a " + pets + " as a pet that keeps " + funFact);
+console.log(pets);
+console.log(funFact);
---
+++ main.js
@@ -0,0 +1,1 @@
+# Web Appliction Main Javascript
