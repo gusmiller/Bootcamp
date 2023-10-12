0428d9d2502dd36559eae0afb22dca12ec4645ba
Gus Miller <gustavo.miller@miller-hs.com>
2023-10-12 18:56:24 -0400

    keep things

--- 10-OOP/22-Stu_Promise-Chain/Unsolved/index.js
+++ 10-OOP/22-Stu_Promise-Chain/Unsolved/index.js
@@ -4,14 +4,19 @@ const { readFile, writeFile } = require('fs/promises');
 const BlogPost = require('./lib/blogPost');
 
 // TODO: First, call readFile to asynchronously read the data from data/post.json.
-readFile('./data/post.json', 'utf-8').then((json) => {
-  console.log('Received data from post.json');
-  // TODO: parse the json string and assign the resulting object to a variable
-  //
-  // TODO: Use the BlogPost class to create a new BlogPost object and use its `render()` method to return an html string.
-  //
-  // TODO: Write the html to file
-  //
-});
+readFile('./data/post.json', 'utf-8')
+  .then((json) => {
+    console.log('Received data from post.json');
+    // TODO: parse the json string and assign the resulting object to a variable
+
+    var obj = JSON.parse(json);
+    // TODO: Use the BlogPost class to create a new BlogPost object and use its `render()` method to return an html string.
+    let o = new BlogPost(obj.title, obj.text, obj.author, obj.createdOn)
+    
+    // TODO: Write the html to file
+    o.render()
+
+  });
 
 // TODO: print a message in the console only after the html file is written.
+console.log('yay!')
